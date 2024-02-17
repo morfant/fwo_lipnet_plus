d99e6e7a382c2e17f7913c1a78eb27712843851f
giy <giy.hands@gmail.com>
2024-02-17 21:20:20 +0900

    Auto start

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+sleep 5.0
+cd /Users && open .
+sleep 3.0
+mount_smbfs //roads22:rx782rx782@MyCloud-0AU6T9._smb._tcp.local/Public/ /Users/NetworkFolder
+sleep 5.0
 /Applications/Max.app/Contents/MacOS/Max /Users/baggeunsu/fwo_lipnet_plus/max_patch/OSC_manager.maxpat &
 /Applications/Resolume\ Arena\ 6/Arena.app/Contents/MacOS/Arena &
-
-export OPENAI_API_KEY=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 &&
-cd /Users/baggeunsu/fwo_lipnet_plus && /Users/baggeunsu/.pyenv/versions/3.11.7/bin/python liveCam.py
+export OPENAI_API_KEY=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 && cd /Users/baggeunsu/fwo_lipnet_plus && /Users/baggeunsu/.pyenv/versions/3.11.7/bin/python liveCam.py
 
